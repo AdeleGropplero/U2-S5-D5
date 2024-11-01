d70546e0812df3e87b17e0eb3427af2b12ac4058
AdeleGropplero <adele.gdt@hotmail.com>
2024-11-01 21:24:41 +0100

    sistemare grandezze e responsive

--- page.js
+++ page.js
@@ -10,3 +10,39 @@ window.addEventListener("scroll", () => {
     button.style.backgroundColor = "black";
   }
 });
+
+/* document.getElementById("m1").classList.add("fade2");
+document.getElementById("m2").classList.add("fade3");
+document.getElementById("m3").classList.add("fade2");
+document.getElementById("m4").classList.add("fade3");
+
+document.getElementById("m5").classList.add("fade2");
+document.getElementById("m6").classList.add("fade3");
+document.getElementById("m7").classList.add("fade2");
+document.getElementById("m8").classList.add("fade3");
+
+document.getElementById("m9").classList.add("fade3");
+document.getElementById("m10").classList.add("fade2");
+document.getElementById("m11").classList.add("fade3");
+document.getElementById("m12").classList.add("fade2"); */
+
+setInterval(() => {
+  document.getElementById("m1").classList.toggle("fade");
+  document.getElementById("m2").classList.toggle("fade");
+  document.getElementById("m3").classList.toggle("fade");
+  document.getElementById("m4").classList.toggle("fade");
+}, 700);
+
+setInterval(() => {
+  document.getElementById("m5").classList.toggle("fade");
+  document.getElementById("m6").classList.toggle("fade");
+  document.getElementById("m7").classList.toggle("fade");
+  document.getElementById("m8").classList.toggle("fade");
+}, 1000);
+
+setInterval(() => {
+  document.getElementById("m9").classList.toggle("fade");
+  document.getElementById("m10").classList.toggle("fade");
+  document.getElementById("m11").classList.toggle("fade");
+  document.getElementById("m12").classList.toggle("fade");
+}, 1700);
